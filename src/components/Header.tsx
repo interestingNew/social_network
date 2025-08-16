7d340ac7768bff32236faef857121a493d535e6b
Pasynkov Evgeny <zhenjapasynkov972@gmail.com>
2025-08-16 11:00:51 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+
+
+export const Header = () => {
+   return <header className='header'>
+   <img src="https://www.unisender.com/ru/blog/wp-content/uploads/2023/09/12.jpg" alt='лого' />
+   </header>
+}
